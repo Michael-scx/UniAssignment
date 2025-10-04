9ca7bba5d76f0ab40ff1e6f3d9c28956ab1258e5
Michael <rustambekyuldashaliyev1@gmail.com>
2025-10-04 15:16:05 +0500

    NoChange

---
+++ wanted_poster.py
@@ -0,0 +1,14 @@
+
+
+a = input("Outlaw's Name : ")
+
+b = input("Known Alias : ")
+
+c = input("Crime Committed : ")
+
+d = input("Last seen Location : ")
+
+e = int(input("Reward amount : "))
+
+print("\n.  WANTED: DEAD OR ALIVE  ")
+print("Name :", a ,"\nAlias :", b , "\nWanted for :", c , "\nLast seen :", d , "\nReward :", e , "\nIssued by : The Sheriff's Office of " ,d)
