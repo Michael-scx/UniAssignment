8f45bab0ab86d82ac01b1fa54f385d187aed9813
Michael <rustambekyuldashaliyev1@gmail.com>
2025-10-07 23:20:16 +0500

    Updated version1

--- wanted_poster.py
+++ wanted_poster.py
@@ -9,6 +9,19 @@ c = input("Crime Committed : ")
 d = input("Last seen Location : ")
 
 e = input("Reward amount : ")
+x = "*" * 40
+h = "-"*40
 
-print("\n  WANTED: DEAD OR ALIVE  ")
-print("Name :", a ,"\nAlias :", b , "\nWanted for :", c , "\nLast seen :", d , "\nReward :", e , "\nIssued by : The Sheriff's Office of " ,d)
+print(f"\n{x}")
+
+print("     WANTED: DEAD OR ALIVE     ")
+
+print(f"\n{x}")
+
+print("\nName :", a ,"\nAlias :", b , "\nWanted for :", c , "\nLast seen :", d)
+print(f"\n{h}")
+print(f"\nReward : {e}")
+print(f"\n{h}")
+print(f"\nIssued by : The Sheriff's Office of {d}")
+
+print(f"\n{x}")
